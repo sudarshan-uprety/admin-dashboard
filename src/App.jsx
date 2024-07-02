0393b328744c635cd20f3eb8f7c1d15b87057fc8
sudarshan-uprety <mr.sudarshanuprety@gmail.com>
2024-07-02 23:16:29 +0545

    learning react router with childer

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,12 @@
-import { Outlet, useRoutes } from 'react-router-dom';
-import Navbar from './Navbar';
-import router from './router/Router';
-import { createBrowserRouter } from 'react-router-dom';
-import Dashboard from "./dashboard/dashboard"
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Navbar from "./Navbar";
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Dashboard />}>
-        <Route
-          path="ok"
-          element={<h1>hello world</h1>}
-        />
-        <Route path="i" element={<h2 >hello world 2</h2>} />
-      </Route>
-    </Routes>
-  )
+    <div>
+      <Outlet />
+      <Navbar />
+    </div>
+  );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import router from "./routers/Router";
 
-import {
-  RouterProvider,
-} from "react-router-dom";
-import { createBrowserRouter } from 'react-router-dom';
-import NavBar from './Navbar.jsx';
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-  },
-]);
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
     <RouterProvider router={router} />
-
-  </>,
-)
+  </React.StrictMode>
+);
