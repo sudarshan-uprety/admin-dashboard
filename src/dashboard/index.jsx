536dd0da18b83ca205f6ca7fa126ab8877577341
sudarshan-uprety <mr.sudarshanuprety@gmail.com>
2024-07-20 19:14:39 +0545

    writing pipleine

--- src/dashboard/index.jsx
+++ src/dashboard/index.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import NavBar from "../NavBar";
-import Products from "../products/Products";
 import { Outlet } from "react-router-dom";
 
 export default function Dashboard() {
