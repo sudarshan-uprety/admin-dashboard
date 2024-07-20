dd9531a228d574e57c28eee1b459027dd1a06127
sudarshan-uprety <mr.sudarshanuprety@gmail.com>
2024-07-20 19:06:23 +0545

    writing pipleine

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import Navbar from "./NavBar";
+import NavBar from "./NavBar";
 
 export default function App() {
   return (
     <div>
       <Outlet />
-      <Navbar />
+      <NavBar />
     </div>
   );
 }
